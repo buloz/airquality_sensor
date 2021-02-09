77a919d3f111b9d50e30f7ca347fadf564b0196e
buloz <lbizeray30@gmail.com>
2021-02-09 09:34:38 +0100

    BME configuration + data recuperation

---
+++ last_version/bsec_iot_example.c
@@ -0,0 +1,259 @@
+/*!
+ * @file bsec_iot_example.c
+ *
+ * @brief
+ * Example for using of BSEC library in a fixed configuration with the BME680 sensor.
+ * This works by running an endless loop in the bsec_iot_loop() function.
+ */
+
+/*!
+ * @addtogroup bsec_examples BSEC Examples
+ * @brief BSEC usage examples
+ * @{*/
+
+/**********************************************************************************************************************/
+/* header files */
+/**********************************************************************************************************************/
+
+#include "bsec_integration.h"
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+#include <sys/time.h>
+#include <string.h>
+
+#include "driver/i2c.h"
+#include "driver/gpio.h"
+#include "driver/uart.h"
+
+#define I2C_ACK      0x0
+#define I2C_NACK     0x1
+
+#define I2C_BUS      0
+#define I2C_SCL      22
+#define I2C_SDA      21
+#define I2C_FREQ     100000
+
+#define ACK_CHECK_EN 1
+
+/**********************************************************************************************************************/
+/* functions */
+/**********************************************************************************************************************/
+
+/*!
+ * @brief           Write operation in either I2C or SPI
+ *
+ * param[in]        dev_addr        I2C or SPI device address
+ * param[in]        reg_addr        register address
+ * param[in]        reg_data_ptr    pointer to the data to be written
+ * param[in]        data_len        number of bytes to be written
+ *
+ * @return          result of the bus communication function
+ */
+int8_t bus_write(uint8_t dev_addr, uint8_t reg_addr, uint8_t *reg_data_ptr, uint16_t data_len)
+{
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (dev_addr) << 1 | I2C_MASTER_WRITE, ACK_CHECK_EN);
+
+    if (reg_addr){
+        i2c_master_write_byte(cmd, reg_addr, ACK_CHECK_EN);
+    }
+
+    if (reg_data_ptr){
+        i2c_master_write(cmd, reg_data_ptr, data_len, ACK_CHECK_EN);        
+    }
+
+    i2c_master_stop(cmd);
+    esp_err_t res = i2c_master_cmd_begin(0, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    
+    return res;
+}
+
+/*!
+ * @brief           Read operation in either I2C or SPI
+ *
+ * param[in]        dev_addr        I2C or SPI device address
+ * param[in]        reg_addr        register address
+ * param[out]       reg_data_ptr    pointer to the memory to be used to store the read data
+ * param[in]        data_len        number of bytes to be read
+ *
+ * @return          result of the bus communication function
+ */
+int8_t bus_read(uint8_t dev_addr, uint8_t reg_addr, uint8_t *reg_data_ptr, uint16_t data_len)
+{
+    if (!data_len){
+        return 1;
+    }
+
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (dev_addr << 1) | I2C_MASTER_WRITE, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, reg_addr, ACK_CHECK_EN);
+    if (!reg_data_ptr)
+        i2c_master_stop(cmd);
+    
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (dev_addr << 1) | I2C_MASTER_READ, ACK_CHECK_EN);
+    if (data_len > 1) i2c_master_read(cmd, reg_data_ptr, data_len-1, I2C_ACK);
+    i2c_master_read_byte(cmd, reg_data_ptr + data_len-1, I2C_NACK);
+    i2c_master_stop(cmd);
+    
+    esp_err_t res = i2c_master_cmd_begin(0, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+
+    return res;
+}
+
+/*!
+ * @brief           System specific implementation of sleep function
+ *
+ * @param[in]       t_ms    time in milliseconds
+ *
+ * @return          none
+ */
+void sleep_loop(uint32_t t_ms)
+{
+    printf("Sleep i2c\n");
+    vTaskDelay(t_ms / portTICK_RATE_MS);
+}
+
+/*!
+ * @brief           Capture the system time in microseconds
+ *
+ * @return          system_current_time    current system timestamp in microseconds
+ */
+int64_t get_timestamp_us()
+{
+    struct timeval time;
+    gettimeofday(&time,0);
+    return time.tv_sec*1e6 + time.tv_usec;
+}
+
+/*!
+ * @brief           Handling of the ready outputs
+ *
+ * @param[in]       timestamp       time in nanoseconds
+ * @param[in]       iaq             IAQ signal
+ * @param[in]       iaq_accuracy    accuracy of IAQ signal
+ * @param[in]       temperature     temperature signal
+ * @param[in]       humidity        humidity signal
+ * @param[in]       pressure        pressure signal
+ * @param[in]       raw_temperature raw temperature signal
+ * @param[in]       raw_humidity    raw humidity signal
+ * @param[in]       gas             raw gas sensor signal
+ * @param[in]       bsec_status     value returned by the bsec_do_steps() call
+ *
+ * @return          none
+ */
+void output_ready(int64_t timestamp, float iaq, uint8_t iaq_accuracy, float temperature, float humidity,
+     float pressure, float raw_temperature, float raw_humidity, float gas, bsec_library_return_t bsec_status,
+     float static_iaq, float co2_equivalent, float breath_voc_equivalent)
+{
+    printf("Test BME680 :\n");
+    printf("iaq: %f, temperature: %f, humidity: %f, pressure: %f", iaq, temperature, humidity, pressure);
+}
+
+/*!
+ * @brief           Load previous library state from non-volatile memory
+ *
+ * @param[in,out]   state_buffer    buffer to hold the loaded state string
+ * @param[in]       n_buffer        size of the allocated state buffer
+ *
+ * @return          number of bytes copied to state_buffer
+ */
+uint32_t state_load(uint8_t *state_buffer, uint32_t n_buffer)
+{
+    // ...
+    // Load a previous library state from non-volatile memory, if available.
+    //
+    // Return zero if loading was unsuccessful or no state was available, 
+    // otherwise return length of loaded state string.
+    // ...
+    return 0;
+}
+
+/*!
+ * @brief           Save library state to non-volatile memory
+ *
+ * @param[in]       state_buffer    buffer holding the state to be stored
+ * @param[in]       length          length of the state string to be stored
+ *
+ * @return          none
+ */
+void state_save(const uint8_t *state_buffer, uint32_t length)
+{
+    // ...
+    // Save the string some form of non-volatile memory, if possible.
+    // ...
+}
+ 
+/*!
+ * @brief           Load library config from non-volatile memory
+ *
+ * @param[in,out]   config_buffer    buffer to hold the loaded state string
+ * @param[in]       n_buffer        size of the allocated state buffer
+ *
+ * @return          number of bytes copied to config_buffer
+ */
+uint32_t config_load(uint8_t *config_buffer, uint32_t n_buffer)
+{
+    // ...
+    // Load a library config from non-volatile memory, if available.
+    //
+    // Return zero if loading was unsuccessful or no config was available, 
+    // otherwise return length of loaded config string.
+    // ...
+    return 0;
+}
+
+/*!
+ * @brief       Main function which configures BSEC library and then reads and processes the data from sensor based
+ *              on timer ticks
+ *
+ * @return      result of the processing
+ */
+int app_main()
+{
+    return_values_init ret;
+    
+    i2c_config_t conf;
+    conf.mode = I2C_MODE_MASTER;
+    conf.sda_io_num = I2C_SDA;
+    conf.scl_io_num = I2C_SCL;
+    conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.master.clk_speed = I2C_FREQ;
+    i2c_param_config(0, &conf);
+    i2c_driver_install(0, I2C_MODE_MASTER, 0, 0, 0);
+
+    printf("I2C master initialized\n");
+    /* Call to the function which initializes the BSEC library 
+     * Switch on low-power mode and provide no temperature offset */
+    ret = bsec_iot_init(BSEC_SAMPLE_RATE_LP, 0.0f, bus_write, bus_read, sleep_loop, state_load, config_load);
+    if (ret.bme680_status)
+    {
+        /* Could not intialize BME680 */
+        printf("Couldn't initialize BME680\n");
+        return (int)ret.bme680_status;
+    }
+    else if (ret.bsec_status)
+    {
+        /* Could not intialize BSEC library */
+        printf("Couldn't initialize BSEC lib\n");
+        return (int)ret.bsec_status;
+    }
+    
+    /* Call to endless loop function which reads and processes data based on sensor settings */
+    /* State is saved every 10.000 samples, which means every 10.000 * 3 secs = 500 minutes  */
+    bsec_iot_loop(sleep_loop, get_timestamp_us, output_ready, state_save, 10000);
+    
+    return 0;
+}
+
+/*! @}*/
+
--- main/component.mk
+++ main/component.mk
@@ -6,3 +6,14 @@
 # in the build directory. This behaviour is entirely configurable,
 # please read the ESP-IDF documents if you need to do this.
 #
+
+COMPONENT_SRCDIRS := .
+COMPONENT_ADD_INCLUDEDIRS := include
+
+LIBS := algobsec
+
+COMPONENT_ADD_LDFLAGS     := -lbt -L $(COMPONENT_PATH) \
+                           $(addprefix -l,$(LIBS))
+ALL_LIB_FILES := $(patsubst %,$(COMPONENT_PATH)/lib%.a,$(LIBS))
+COMPONENT_ADD_LINKER_DEPS := $(ALL_LIB_FILES)
+COMPONENT_SUBMODULES += lib
